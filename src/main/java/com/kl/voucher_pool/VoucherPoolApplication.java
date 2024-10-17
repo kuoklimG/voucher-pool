4950440a543d3a73240e5b1e8d0ce2e8b090a32a
kuoklim <kuoklim21@gmail.com>
2024-10-17 15:24:52 +0800

    Initial commit

---
+++ src/main/java/com/kl/voucher_pool/VoucherPoolApplication.java
@@ -0,0 +1,13 @@
+package com.kl.voucher_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherPoolApplication.class, args);
+	}
+
+}
