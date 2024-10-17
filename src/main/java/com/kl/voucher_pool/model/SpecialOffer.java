c1ce6a8c2602514910cd0f7ce9616244798928fe
kuoklim <kuoklim21@gmail.com>
2024-10-17 16:03:55 +0800

    Add Recipient, SpecialOffer and VoucherCode model.

---
+++ src/main/java/com/kl/voucher_pool/model/SpecialOffer.java
@@ -0,0 +1,41 @@
+package com.example.voucherpool.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "special_offers")
+public class SpecialOffer {
+    @Id
+    private String id;
+    private String name;
+    private double discountPercentage;
+
+    public SpecialOffer(String name, double discountPercentage) {
+        this.name = name;
+        this.discountPercentage = discountPercentage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+}
---
+++ src/main/java/com/kl/voucher_pool/model/VoucherCode.java
@@ -0,0 +1,72 @@
+package com.example.voucherpool.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Document(collection = "voucher_codes")
+public class VoucherCode {
+    @Id
+    private String id;
+    private String code;
+    private String recipientId;
+    private String specialOfferId;
+    private LocalDate expirationDate;
+    private LocalDate usageDate;
+
+    public VoucherCode(String code, String recipientId, String specialOfferId, LocalDate expirationDate) {
+        this.code = code;
+        this.recipientId = recipientId;
+        this.specialOfferId = specialOfferId;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(String recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getSpecialOfferId() {
+        return specialOfferId;
+    }
+
+    public void setSpecialOfferId(String specialOfferId) {
+        this.specialOfferId = specialOfferId;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public LocalDate getUsageDate() {
+        return usageDate;
+    }
+
+    public void setUsageDate(LocalDate usageDate) {
+        this.usageDate = usageDate;
+    }
+}
